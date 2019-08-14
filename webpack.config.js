58db094838e1a9c3d97295001a4294cb0fe5953e
luoqi <399599243@qq.com>
2019-08-14 18:00:06 +0800

    使用样式和颜色

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');
+module.exports = {
+    entry:'./index.js',
+    output: {
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: {
+                   loader:'babel-loader',
+                    options: {
+                       presets:['es2015']
+                    }
+                }
+            }
+        ]
+    }
+}
